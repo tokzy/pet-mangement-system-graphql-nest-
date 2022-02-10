99db5138fca9388824eb1d0500a6ca81efdec107
netsage <isaiahtokunbo11@gmail.com>
2022-02-10 10:14:26 +0100

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,  
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.dbHost,
+      port: parseInt(process.env.dbPort),
+      username: process.env.dbUser,
+      password: process.env.dbPass,
+      database: process.env.dbName,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule],
   controllers: [],
   providers: [],
 })
